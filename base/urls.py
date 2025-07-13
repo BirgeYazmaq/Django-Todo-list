676c3ed18efded63cbe4d06989a23a5c1501a669
valiyyaddin <eliyevveliyeddin3@gmail.com>
2025-07-13 14:04:21 +0400

    Create CRUD

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, TaskDelete
 
 from . import views
 
 urlpatterns = [
     path('',TaskList.as_view(),name='tasks'),
     path('task/<int:pk>',TaskDetail.as_view(), name='task'),
+    path('create-task/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>', TaskDelete.as_view(), name='task-delete'),
 
 ]
