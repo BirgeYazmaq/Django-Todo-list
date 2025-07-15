127b1d03646583ec961eb98a5d110639d7c94937
valiyyaddin <eliyevveliyeddin3@gmail.com>
2025-07-15 12:51:37 +0400

    Configure remote-postegre sql using railway.app

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        ##'ENGINE': 'django.db.backends.sqlite3',
+        ##'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ZnqQGPgNcTJUVXQnvWemPrDXfxBspQnf',
+        'HOST': 'yamabiko.proxy.rlwy.net',
+        'PORT': '38775',
     }
 }
 
